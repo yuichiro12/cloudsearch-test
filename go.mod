0dcb7a76dcec016d47d4f6c9e8e19c704c73c75a
yuichiro12 <talcnrhyme@gmail.com>
2019-08-31 02:20:08 +0900

    cloudsearch server

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module "https://github.com/yuichiro12/cloudsearch-test"
 
 go 1.12
 
-require github.com/aws/aws-sdk-go v1.23.12
+require (
+	github.com/aws/aws-sdk-go v1.23.12
+	github.com/davecgh/go-spew v1.1.1
+	github.com/gorilla/mux v1.7.3
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
-	"github.com/aws/aws-sdk-go/service/cloudsearch"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudsearchdomain"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	cs := cloudsearch.New()
+	// CloudSearchDomain methods are safe to use concurrently. It is not safe to
+	// modify mutate any of the struct's properties though.
+	cs := NewCloudSearchDomainClient()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		val := r.URL.Query()
+		q := val.Get("q")
+		input := &cloudsearchdomain.SearchInput{Query: &q}
+		output, err := cs.Search(input)
+		if err != nil {
+			if _, err := io.WriteString(w, err.Error()); err != nil {
+				log.Fatal(err)
+			}
+		} else if _, err := io.WriteString(w, output.String()); err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":8443", router))
+}
+
+func NewCloudSearchDomainClient() *cloudsearchdomain.CloudSearchDomain {
+	ep := os.Getenv("CLOUDSEARCHDOMAIN_SEARCH_ENDPOINT")
+	config := aws.NewConfig().WithRegion("us-west-1").WithEndpoint(ep)
+	sess, err := session.NewSession(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return cloudsearchdomain.New(sess)
 }
